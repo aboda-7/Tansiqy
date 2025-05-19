9e5fee46a84954f74b442e9fe4d277e542303e11
Abdalla Sameh <154898979+aboda-7@users.noreply.github.com>
2025-05-19 20:38:05 +0300

    Delete Frontend directory

--- Frontend/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TansiqyFrontend.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Frontend/ViewModels/FacultyViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TansiqyFrontend.ViewModels
-{
-    public class FacultyViewModel
-    {
-        public int FID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
